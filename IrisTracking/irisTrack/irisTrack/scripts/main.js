c8a255527bc7b7653d5b7084dbf3e23d3048f3e0
elchangodiaz <elchangodiaz1@gmail.com>
2022-03-14 00:17:23 -0600

    se agregan rastreo de ojos

--- IrisTracking/irisTrack/irisTrack/scripts/main.js
+++ IrisTracking/irisTrack/irisTrack/scripts/main.js
@@ -1,3 +1,5 @@
+import { leftEyeball } from 'IrisTracking';
+
 const Scene = require('Scene');
 const FaceTracking = require('FaceTracking');
 const IrisTracking = require('IrisTracking');
@@ -6,12 +8,34 @@ export const Diagnostics = require('Diagnostics');
 
 (async function () { 
 
-  const [face,centerPlane, irisPlane] = await Promise.all([
+  const [face,ojoDPlane, ojoIPlane] = await Promise.all([
     FaceTracking.face(0),
-    Scene.root.findFirst('plane0'),
-    Scene.root.findFirst('plane1')
+    Scene.root.findFirst('ojoD'),
+    Scene.root.findFirst('ojoI')
   ]);
   
+  const leftEyeball = IrisTracking.leftEyeball(face);
+  const rightEyeball = IrisTracking.rightEyeball(face);
+
+  const ojoDPlaneTransform = ojoDPlane.transform;
+  const ojoIPlaneTransform = ojoIPlane.transform;
+
+  const leftEyeballCenter = leftEyeball.center;
+  const leftEyeballIris = leftEyeball.iris;
+  const leftEyeballRotation = leftEyeball.rotation;
+
+  const rightEyeballCenter = rightEyeball.center;
+  const rightEyeballIris = rightEyeball.iris;
+  const rightEyeballRotation = rightEyeball.rotation;
+
+  ojoDPlaneTransform.x = rightEyeballIris.x;
+  ojoDPlaneTransform.y = rightEyeballIris.y;
+  ojoDPlaneTransform.z = rightEyeballIris.z;
+
+  ojoIPlaneTransform.x = leftEyeballIris.x;
+  ojoIPlaneTransform.y = leftEyeballIris.y;
+  ojoIPlaneTransform.z = leftEyeballIris.z;
+
   
 
 
